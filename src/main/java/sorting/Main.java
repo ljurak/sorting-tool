b87da13eea5deb0b99112bee6efd94ebb837ddb6
ljurak <lucas.jurak@gmail.com>
2020-07-02 04:45:19 +0200

    Add user input error handling

--- src/main/java/sorting/Main.java
+++ src/main/java/sorting/Main.java
@@ -11,12 +11,24 @@ public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    private static String[] validArgumentNames = new String[] {"-dataType", "-sortingType"};
+
+    private static String[] validDataTypes = new String[] {"long", "line", "word"};
+
+    private static String[] validSortingTypes = new String[] {"natural", "byCount"};
+
+    private static boolean validArguments = true;
+
     private static String dataType = "word";
 
     private static String sortingType = "natural";
 
     public static void main(String[] args) {
         processArguments(args);
+
+        if (!validArguments) {
+            return;
+        }
 
         switch (dataType) {
             case "long":
@@ -28,42 +40,86 @@ public class Main {
             case "word":
                 processWords();
                 break;
-            default:
-                throw new IllegalArgumentException("Unsupported dataType value: " + dataType);
         }
     }
 
-    private static void processArguments(String[] args) {
-        String currentArg = null;
-        for (String arg : args) {
-            if (arg.startsWith("-")) {
-                currentArg = arg;
+    private static void processArguments(String[] arguments) {
+        String currentArgumentName = null;
+        boolean isArgumentValueExpected = false;
+
+        for (String argument : arguments) {
+            if (isArgumentValueExpected) {
+                if (argument.startsWith("-")) {
+                    showMissingArgument(currentArgumentName);
+                    validArguments = false;
+                    return;
+                } else {
+                    assignArgument(currentArgumentName, argument);
+                    isArgumentValueExpected = false;
+                }
             } else {
-                assignArgument(currentArg, arg);
+                if (argument.startsWith("-")) {
+                    if (validateArgumentName(argument)) {
+                        currentArgumentName = argument;
+                        isArgumentValueExpected = true;
+                    } else {
+                        System.out.println(argument + " isn't a valid parameter. It's skipped.");
+                    }
+                }
             }
         }
 
-        if (!(sortingType.equals("natural") || sortingType.equals("byCount"))) {
-            throw new IllegalArgumentException("Unsupported sortingType value: " + sortingType);
+        if (isArgumentValueExpected) {
+            showMissingArgument(currentArgumentName);
+            validArguments = false;
         }
     }
 
-    private static void assignArgument(String argName, String argValue) {
-        if (argName == null) {
-            return;
+    private static boolean validateArgumentName(String name) {
+        for (String validArgument : validArgumentNames) {
+            if (validArgument.equals(name)) {
+                return true;
+            }
         }
+        return false;
+    }
 
-        if ("-dataType".equals(argName)) {
-            dataType = argValue;
-        } else if ("-sortingType".equals(argName)) {
-            sortingType = argValue;
+    private static boolean validateArgumentValue(String name, String value) {
+        if ("-dataType".equals(name)) {
+            return arrayContains(validDataTypes, value);
+        } else if ("-sortingType".equals(name)) {
+            return arrayContains(validSortingTypes, value);
+        }
+        return false;
+    }
+
+    private static void assignArgument(String name, String value) {
+        if (validateArgumentValue(name, value)) {
+            if ("-dataType".equals(name)) {
+                dataType = value;
+            } else if ("-sortingType".equals(name)) {
+                sortingType = value;
+            }
+        }
+    }
+
+    private static void showMissingArgument(String argument) {
+        if ("-dataType".equals(argument)) {
+            System.out.println("No data type defined!");
+        } else if ("-sortingType".equals(argument)) {
+            System.out.println("No sorting type defined!");
         }
     }
 
     private static void processLongs() {
         List<Long> numbers = new ArrayList<>();
-        while (scanner.hasNextLong()) {
-            numbers.add(scanner.nextLong());
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            try {
+                numbers.add(Long.parseLong(next));
+            } catch (NumberFormatException e) {
+                System.out.println("\"" + next + "\" isn't a long. It's skipped.");
+            }
         }
 
         System.out.println("Total numbers: " + numbers.size() + ".");
@@ -116,5 +172,14 @@ public class Main {
         Map<T, Integer> result = new HashMap<>();
         data.forEach(el -> result.merge(el, 1, Integer::sum));
         return result;
+    }
+
+    private static <T> boolean arrayContains(T[] array, T value) {
+        for (T el : array) {
+            if (value.equals(el)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
