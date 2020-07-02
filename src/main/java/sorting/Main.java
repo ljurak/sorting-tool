c24dd3c844ce4572ee6ee741ac917c424127765f
ljurak <lucas.jurak@gmail.com>
2020-07-02 11:55:55 +0200

    Extract code for processing arguments to Arguments class

--- src/main/java/sorting/Main.java
+++ src/main/java/sorting/Main.java
@@ -11,26 +11,16 @@ public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    private static String[] validArgumentNames = new String[] {"-dataType", "-sortingType"};
-
-    private static String[] validDataTypes = new String[] {"long", "line", "word"};
-
-    private static String[] validSortingTypes = new String[] {"natural", "byCount"};
-
-    private static boolean validArguments = true;
-
-    private static String dataType = "word";
-
-    private static String sortingType = "natural";
+    private static final Arguments arguments = new Arguments();
 
     public static void main(String[] args) {
-        processArguments(args);
+        arguments.processArguments(args);
 
-        if (!validArguments) {
+        if (!arguments.isValidArguments()) {
             return;
         }
 
-        switch (dataType) {
+        switch (arguments.getDataType()) {
             case "long":
                 processLongs();
                 break;
@@ -40,74 +30,6 @@ public class Main {
             case "word":
                 processWords();
                 break;
-        }
-    }
-
-    private static void processArguments(String[] arguments) {
-        String currentArgumentName = null;
-        boolean isArgumentValueExpected = false;
-
-        for (String argument : arguments) {
-            if (isArgumentValueExpected) {
-                if (argument.startsWith("-")) {
-                    showMissingArgument(currentArgumentName);
-                    validArguments = false;
-                    return;
-                } else {
-                    assignArgument(currentArgumentName, argument);
-                    isArgumentValueExpected = false;
-                }
-            } else {
-                if (argument.startsWith("-")) {
-                    if (validateArgumentName(argument)) {
-                        currentArgumentName = argument;
-                        isArgumentValueExpected = true;
-                    } else {
-                        System.out.println(argument + " isn't a valid parameter. It's skipped.");
-                    }
-                }
-            }
-        }
-
-        if (isArgumentValueExpected) {
-            showMissingArgument(currentArgumentName);
-            validArguments = false;
-        }
-    }
-
-    private static boolean validateArgumentName(String name) {
-        for (String validArgument : validArgumentNames) {
-            if (validArgument.equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean validateArgumentValue(String name, String value) {
-        if ("-dataType".equals(name)) {
-            return arrayContains(validDataTypes, value);
-        } else if ("-sortingType".equals(name)) {
-            return arrayContains(validSortingTypes, value);
-        }
-        return false;
-    }
-
-    private static void assignArgument(String name, String value) {
-        if (validateArgumentValue(name, value)) {
-            if ("-dataType".equals(name)) {
-                dataType = value;
-            } else if ("-sortingType".equals(name)) {
-                sortingType = value;
-            }
-        }
-    }
-
-    private static void showMissingArgument(String argument) {
-        if ("-dataType".equals(argument)) {
-            System.out.println("No data type defined!");
-        } else if ("-sortingType".equals(argument)) {
-            System.out.println("No sorting type defined!");
         }
     }
 
@@ -147,6 +69,7 @@ public class Main {
     }
 
     private static <T extends Comparable<? super T>> void processSorting(List<T> data, boolean printMultiline) {
+        String sortingType = arguments.getSortingType();
         if (data.size() > 0) {
             if ("natural".equals(sortingType)) {
                 Collections.sort(data);
@@ -172,14 +95,5 @@ public class Main {
         Map<T, Integer> result = new HashMap<>();
         data.forEach(el -> result.merge(el, 1, Integer::sum));
         return result;
-    }
-
-    private static <T> boolean arrayContains(T[] array, T value) {
-        for (T el : array) {
-            if (value.equals(el)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
